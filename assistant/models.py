5f2bb5c07b543196760a250972d13efb15dd3f33
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-21 12:21:44 +0100

    create && migrate Bug models

--- assistant/models.py
+++ assistant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bug(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE ,related_name='bugs')
+
+    def __str__(self):
+        return self.title
